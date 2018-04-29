59b0fc2c73c5546598421677d1a8884373cbe952
Martin Farstad <farstad2k@gmail.com>
2018-04-29 15:57:22 +0200

    Add exception handling

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -5,6 +5,7 @@ import { Observable } from "rxjs/Observable";
 import { of } from "rxjs/observable/of";
 import { MessageService } from "./message.service";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { catchError, map, tap } from "rxjs/operators";
 
 @Injectable()
 export class TodoService {
@@ -20,6 +21,30 @@ export class TodoService {
   }
 
   getTodos(): Observable<Todo[]> {
-    return this.http.get<Todos[]>(this.todosUrl);
+    return this.http
+      .get<Todos[]>(this.todosUrl)
+      .pipe(
+        tap(todos => this.log(`fetched todos`)),
+        catchError(this.handleError("getTodos", []))
+      );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = "operation", result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
